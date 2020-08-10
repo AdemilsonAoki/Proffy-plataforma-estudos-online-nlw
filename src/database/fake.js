34e3a9f44f73bdee18522ce1b9276735b6942638
Ademilson <ademilson.aoki@gmai.com>
2020-08-10 00:09:29 -0300

    Banco de dados, cadastro e consulta

---
+++ src/database/fake.js
@@ -0,0 +1,24 @@
+const proffys =[
+    {
+        name:"Diego Fernandes", 
+        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4"  ,
+        whatsapp:"89987654534", 
+         bio: "Entusiasta das melhores tecnologias de química avançada.", 
+         subject:"História", 
+         cost :"20",
+          weekday:[0], 
+          time_fro:[720] , 
+          time_to:[1220] 
+    },
+    {
+        name:"Daniele Fernandes", 
+        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4"  ,
+        whatsapp:"89987654534", 
+         bio: "Entusiasta das melhores tecnologias de química avançada.", 
+         subject:"História", 
+         cost :"20",
+          weekday:[1], 
+          time_fro:[720] , 
+          time_to:[1220] 
+    }
+]
--- src/server.js
+++ src/server.js
@@ -1,116 +1,20 @@
-const proffys =[
-    {
-        name:"Diego Fernandes", 
-        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4"  ,
-        whatsapp:"89987654534", 
-         bio: "Entusiasta das melhores tecnologias de química avançada.", 
-         subject:"História", 
-         cost :"20",
-          weekday:[0], 
-          time_fro:[720] , 
-          time_to:[1220] 
-    },
-    {
-        name:"Daniele Fernandes", 
-        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4"  ,
-        whatsapp:"89987654534", 
-         bio: "Entusiasta das melhores tecnologias de química avançada.", 
-         subject:"História", 
-         cost :"20",
-          weekday:[1], 
-          time_fro:[720] , 
-          time_to:[1220] 
-    }
-]
-    const subjects = [
+const express = require('express');
+const server = express();
 
-        "Artes",
-        "Biologia",
-        "Ciências",
-        "Educação física",
-        "Física",
-        "Geografia",
-        "História",
-        "Matemática",
-        "Português",
-        "Química",
-    ]
+const { pageLanding, pageStudy, pageGiveClasses, saveClasses } = require('./pages')
 
-    const weekdays = [
-
-        "Domingo",
-        "Segunda-feira",
-         "Terça-feira",
-        "Quarta-feira",
-        "Quinta-feira",
-        "Sexta-feira",
-        "Sábado",
-    ]
-
-
-    function getSubject(subjectNumber){
-        const position = +subjectNumber-1
-        return subjects[position]
-
-    }
-
-    
-
-
-function pageLading(req, res){
-    return res.render("index.html")
-    
-}
-
-function pageStudy(req, res){
-    const filters = req.query
-
-
-
-    return res.render("study.html",{ proffys, filters, subjects, weekdays})
-        
-}
-function pageGiveClasses(req, res){
-    const data = req.query
-
-    const isNotEmpty = Object.keys(data).length != 0
-
-
-    if(isNotEmpty){
-        data.subject = getSubject(data.subject)
-
-        proffys.push(data)
-        return res.redirect("/study")
-    }
-    
-
-
-
-
-    return res.render("give-classes.html", {subjects, weekdays})
-
-}
-
-
-
-const express = require('express')
-const server = express()
-
-
-const nunjucks = require('nunjucks')
-
-nunjucks.configure('src/views',{
-    express: server,
-    noCache: true
+const nunjucks = require('nunjucks');
+nunjucks.configure('src/views', {
+  express: server,
+  noCache: true,
 })
 
+server
+.use(express.urlencoded({ extended: true }))
+.use(express.static("public"))
+.get("/", pageLanding)
+.get("/study", pageStudy)
+.get("/give-classes", pageGiveClasses)
+.post("/save-classes", saveClasses)
 
-server.use(express.static('public'))
-.get("/", pageLading) 
-.get("/study", pageStudy) 
-.get("/give-classes", pageGiveClasses) 
-
-
-  
-.listen(5500)
-
+.listen(5500);
